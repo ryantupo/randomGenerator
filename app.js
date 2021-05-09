d264becf4a31216b7384e01dd4002dd04a4c9780
Ryan <ryantupo98@gmail.com>
2021-05-09 19:26:42 +0100

    09.05.2021 api working

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
 const app = Vue.createApp({
-    template: '<h1>Hello World</h1>'
+    data() {
+        return {
+            firstName: 'John',
+            lastName: 'Doe',
+            email: 'John@gmail.com',
+            gender: 'male',
+            picture: 'https://randomuser.me/api/portraits/men/10.jpg',
+        }
+    },
+    methods: {
+        async getUser(){
+            const res = await fetch('https://randomuser.me/api')
+
+            const { results } = await res.json()
+
+            this.firstName = results[0].name.firstName
+            this.lastName = results[0].name.lastName
+            this.email = results[0].email
+            this.gender = results[0].gender
+            this.picture = results[0].picture.large
+        }
+    },
 })
 
 app.mount('#app')
